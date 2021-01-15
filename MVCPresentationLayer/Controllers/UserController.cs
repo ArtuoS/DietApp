bea3779167e9fa473078423350fcf39bc51bcd0a
Arthur Molinari <arthureduardomolinari@gmail.com>
2021-01-15 10:55:54 -0300

    UP

---
+++ MVCPresentationLayer/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCPresentationLayer.Models.InsertModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCPresentationLayer.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Insert()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Insert(UserInsertViewModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCPresentationLayer/Models/InsertModels/UserInsertViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCPresentationLayer.Models.InsertModels
+{
+    public class UserInsertViewModel
+    {
+		public Food_Category Category { get; set; }
+
+		[Required]
+		public string Food_Name { get; set; }
+
+		public double Calories { get; set; }
+		public double Alcohol { get; set; }
+		public double Carbohydrate { get; set; }
+		public double Copper { get; set; }
+		public double Fiber { get; set; }
+		public double Lipid { get; set; }
+		public double Magnesium { get; set; }
+		public double Phosphor { get; set; }
+		public double Potassium { get; set; }
+		public double Protein { get; set; }
+		public double Sodium { get; set; }
+		public double Selenium { get; set; }
+		public double Vitamin_A { get; set; }
+		public double Vitamin_C { get; set; }
+		public double Vitamin_D { get; set; }
+		public double Vitamin_E { get; set; }
+		public double Vitamin_B6 { get; set; }
+		public double Vitamin_B12 { get; set; }
+		public double Zinc { get; set; }
+	}
+}
